40d21c28dc36293852280ef5a85c42e394fe028a
noname <46762733+kowalst@users.noreply.github.com>
2019-11-06 14:41:29 -0800

    Update TagManager.java

--- TagManager.java
+++ TagManager.java
@@ -1,2 +1,50 @@
+//Author: Thomas Kowalski
+//Class: CS145
+//Purpose: Assasin game
+//Date: 11/6/19
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class TagManager {
+  
 }
+
+/* breaks in pdf */
+
+/* 
+Linked node class has two members: name to store the name if the player and the next to get the 
+reference of the next player
+class LinkedNode
+{
+*/
+
+/*
+int number = rn.nextint(count):
+walker = firstLink;
+//set prevLink to the firstLink
+prevLink = firstLink;
+*/
+
+/*
+boolen namefound;
+//while count is greater than two
+while(count>2)
+{
+*/
+
+/*
+//tempLink.next
+prevLink.next = tempLink.next;
+//set the deletLink.next to null
+*/
+
+/*
+tempLink=tempLink.next;
+}
+}
+addGraveyardList() method adds the input linked node to the list of persons who are killed 
+in the game
+*/
